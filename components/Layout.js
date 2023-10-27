9c2b5dadabfedd2fccd564466aa7244c0ad3608f
Abdul Rafay <abdulrafayn001gmail.com>
2023-10-27 23:57:34 +0500

    feat: Add components and animations to the Home page
    - Added ProjectsBtn and Avatar components
    - Implemented Framer Motion animations for page elements
    - Updated the Home page layout and styles
    - Included a background image and particles
    - Modified the avatar image

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,26 @@
-const Layout = () => {
-  return <div>Layout</div>;
+import { Sora } from "@next/font/google";
+
+const sora = Sora({
+  subsets: ["latin"],
+  variable: "--font-sora",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
+
+import Nav from "./Nav";
+import Header from "./Header";
+import TopLeftImage from "./TopLeftImg";
+
+const Layout = ({ children }) => {
+  return (
+    <div
+      className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}
+    >
+      <TopLeftImage />
+      <Nav />
+      <Header />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
