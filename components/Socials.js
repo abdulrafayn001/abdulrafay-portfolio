28a21a177c15a63fde9e1af7e9da516cda0e6f57
Abdul Rafay <abdulrafayn001gmail.com>
2023-10-28 19:44:38 +0500

    feat: Add header and social media links
    - Updated the Header component with the logo and social media links
    - Created the Socials component to display social media icons
    - Linked social media icons to placeholder URLs

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,51 @@
+import Link from "next/link";
+import {
+  RiYoutubeLine,
+  RiInstagramLine,
+  RiTwitterXLine,
+  RiGithubLine,
+  RiLinkedinLine,
+} from "react-icons/ri";
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link
+        href={"https://github.com/abdulrafayn001"}
+        target="_blank"
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiGithubLine />
+      </Link>
+      <Link
+        href={"https://www.linkedin.com/in/abdulrafayn001/"}
+        target="_blank"
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiLinkedinLine />
+      </Link>
+      <Link
+        href={"https://twitter.com/abdulrafayn001"}
+        target="_blank"
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiTwitterXLine />
+      </Link>
+      <Link
+        href={"https://www.instagram.com/im_abdulrafay"}
+        target="_blank"
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiInstagramLine />
+      </Link>
+      <Link
+        href={"https://www.youtube.com/channel/UCl9zBPMQ1X4KeecQlf-0omA"}
+        target="_blank"
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiYoutubeLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
