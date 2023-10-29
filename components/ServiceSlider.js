8d10e0bba5fbbad5b636cf2101742aa7d7ff025b
Abdul Rafay <abdulrafayn001gmail.com>
2023-10-29 17:12:21 +0500

    feat: Enhance Services page with dynamic content and components
    - Added Bulb component for visual effects
    - Implemented a ServiceSlider component for displaying services
    - Added animation and visual elements to the Services page
    - Updated Service data with icons and descriptions
    - Created a new PNG image for the Bulb component

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,44 +1,91 @@
-// icons
-import {
-  RxCrop,
-  RxPencil2,
-  RxDesktop,
-  RxReader,
-  RxRocket,
-} from "react-icons/rx";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+// icons
+import { RxReader, RxArrowTopRight, RxCode } from "react-icons/rx";
 
 // data
 const serviceData = [
   {
-    icon: <RxCrop />,
-    title: 'Branding',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxPencil2 />,
-    title: 'Design',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxDesktop />,
-    title: 'Development',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    icon: <RxCode />,
+    title: "Development",
+    description:
+      "Crafting responsive, user-friendly websites tailored to your unique vision.",
   },
   {
     icon: <RxReader />,
-    title: 'Copywriting',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Database",
+    description:
+      "Expertly handling data, from design to integration, ensuring efficiency..",
   },
   {
-    icon: <RxRocket />,
-    title: 'SEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    icon: <HiServer />,
+    title: "DevOps",
+    description:
+      "Streamlining development and deployment pipelines for efficiency",
+  },
+  {
+    icon: <HiCloud />,
+    title: "Cloud Solutions",
+    description:
+      "Leveraging cloud services for scalable and cost-effective applications.",
+  },
+  {
+    icon: <HiLightBulb />,
+    title: "Consulting",
+    description:
+      "Providing expert guidance and insights to elevate your software projects.",
   },
 ];
 
+// import required modules
+import { FreeMode, Pagination } from "swiper";
+import { HiCloud, HiLightBulb, HiServer } from "react-icons/hi2";
+
 const ServiceSlider = () => {
-  return <div>Service Slider</div>;
+  return (
+    <Swiper
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+      }}
+      freeMode={true}
+      pagination={{ clickable: true }}
+      modules={[FreeMode, Pagination]}
+      className="h-[240px] sm:h-[340px]"
+    >
+      {serviceData.map((item, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className="bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300">
+              {/* icon */}
+              <div className="text-4xl text-accent mb-4">{item.icon}</div>
+              {/* title & desc */}
+              <div className="mb-8">
+                <div className="mb-2 text-lg">{item.title}</div>
+                <p className="max-w-[350px] leading-normal">
+                  {item.description}
+                </p>
+              </div>
+              {/* arrow */}
+              <div className="text-3xl">
+                <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-accent transition-all duration-300" />
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default ServiceSlider;
--- pages/services/index.js
+++ pages/services/index.js
@@ -1,5 +1,54 @@
+import Circles from "../../components/Circles";
+import ServicesSlider from "../../components/ServiceSlider";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+import Bulb from "../../components/Bulb";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          {/* text */}
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <motion.h2
+              variants={fadeIn("up", 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-8"
+            >
+              My services <span className="text-accent">.</span>
+            </motion.h2>
+            <motion.p
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+            >
+              From web development to consulting, DevOps integration, database
+              management, and cloud solutions, I offer expertise to elevate your
+              success. Explore how I can help you reach new heights in the
+              digital landscape.
+            </motion.p>
+          </div>
+          {/* sidebar */}
+          <motion.div
+            variants={fadeIn("up", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[65%]"
+          >
+            <ServicesSlider />
+          </motion.div>
+        </div>
+      </div>
+      <Bulb />
+    </div>
+  );
 };
 
 export default Services;
